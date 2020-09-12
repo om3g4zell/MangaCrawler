20ef7036ef9b066130f8b0fad277f02cb51d14e1
Louis <louisb79000@gmail.com>
2020-09-12 18:33:28 +0200

    Project configuration

---
+++ src/main/java/com/om3g4zell/MangaCrawler.java
@@ -0,0 +1,13 @@
+package com.om3g4zell;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MangaCrawler {
+
+    private static final Logger logger = LogManager.getLogger(MangaCrawler.class);
+
+    public static void main(String[] args) {
+        logger.warn("Hello guys !");
+    }
+}
